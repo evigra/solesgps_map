9ca3a711ed31b66bd45277c6535e354b5750ed3f
Eduardo Vizcaino <evigra@hotmail.com>
2020-02-05 18:44:59 -0600

    Update

--- solesgps_map/models/gpsmap.py
+++ solesgps_map/models/gpsmap.py
@@ -28,10 +28,10 @@ class vehicle(models.Model):
         print('CREATE LALO====================')        
 
         #sql="INSERT INTO tc_devices (name,uniqueid,phone,disabled,speed) VALUES ('",vals["name"]"','",vals["uniqueid"],"','",vals["phone"],"','",vals["disabled"],"','",vals["speed"],"')"
-        sql="INSERT INTO tc_devices (name,uniqueid,phone,speed) VALUES ('",vals["name"]"','",vals["uniqueid"],"','",vals["phone"],"','",vals["speed"],"')"
+        #sql="INSERT INTO tc_devices (name,uniqueid,phone,speed) VALUES ('",vals["name"]"','",vals["uniqueid"],"','",vals["phone"],"','",vals["speed"],"')"
         #sql="INSERT INTO tc_devices VALUES ('id','name','uniqueid','lastupdate','positionid','groupid','attributes','phone','model','contact','category','disabled','speed')"
         #vals = {'name': 'ABC', 'standard':10}
-        print(sql)        
+        #print(sql)        
         return super(vehicle, self).create(vals)
     
 class speed(models.Model):
