8ec20411f5f87fb945349b9becac869278f9a664
Eduardo Vizcaino <evigra@hotmail.com>
2020-03-05 17:56:06 -0600

    Add

--- solesgps_map/models/gpsmap.py
+++ solesgps_map/models/gpsmap.py
@@ -70,7 +70,7 @@ class positions(models.Model):
         if len(vehicle_data)>0:         
             for vehicle in vehicle_data:
                 positions_arg                   =[[],[('deviceid','=',vehicle.id)]]
-                positions_arg                   =[[],[]]
+                positions_arg                   =[]
                 positions_data                  =self.search_read(positions_arg, offset=0, limit=1, order='devicetime DESC')
                 if len(positions_data)>0:
                     return_positions[vehicle.id]    =positions_data
