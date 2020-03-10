5480374196dc88eb2a2d9431c9f12236388a4537
Eduardo Vizcaino <evigra@hotmail.com>
2020-03-10 13:12:06 -0600

    Add

--- solesgps_map/static/src/js/index.js
+++ solesgps_map/static/src/js/index.js
@@ -247,6 +247,7 @@ odoo.define('solesgps_map', function(require){
                         method: method,
                         domain: arg,
                         fields: fields_select,
+                        limit:1
                         
                     })
                     
