177175093bb169e6eabf64c17c52550d4ab74fe1
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-28 21:32:35 -0500

    Update

--- solesgps_map/models/gpsmap.py
+++ solesgps_map/models/gpsmap.py
@@ -88,8 +88,9 @@ class positions(models.Model):
                 #if len(positions_data)>0:
                     #print('====== ', positions_data[0])
                     #return_positions[vehicle.id]    =positions_data[0]
-            return return_positions
+            
         """
+        return return_positions
     def run_scheduler_demo(self):
         positions_obj                           =self.env['gpsmap.positions']        
         vehicle_obj                             =self.env['fleet.vehicle']
