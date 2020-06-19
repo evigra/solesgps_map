4283fce3cdfbee4f34ccffe7fe64d098a544b343
Eduardo Vizcaino <evigra@hotmail.com>
2020-06-19 18:50:47 -0500

    Update

--- solesgps_map/models/gpsmap.py
+++ solesgps_map/models/gpsmap.py
@@ -19,8 +19,10 @@ class contract(models.Model):
     _inherit = "fleet.vehicle.log.contract"
 class odometer(models.Model):
     _inherit = "fleet.vehicle.odometer"
-class vehicle_modle(models.Model):
+class vehicle_model(models.Model):
     _inherit = "fleet.vehicle.model"
+class vehicle_model_brand(models.Model):
+    _inherit = "fleet.vehicle.model.brand"
 
 
 
